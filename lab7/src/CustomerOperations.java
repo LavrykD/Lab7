ff3327a09b625556024e2ece321108b78e2bd31a
Dmytro Lavryk <dlavryk1011@gmail.com>
2020-12-20 17:18:16 +0200

    Adding new class CustomerOperations

---
+++ lab7/src/CustomerOperations.java
@@ -0,0 +1,17 @@
+public class CustomerOperations {
+    public static void withdraw(AbstractCustomer customer, double sum, String currency) {
+        Account account = customer.getAccount();
+        if (!account.getMoney().getCurrency().equals(currency)) {
+            throw new RuntimeException("Can't extract withdraw " + currency);
+        }
+        withdraw(account, sum, customer.getOverdraftDiscount());
+    }
+
+    private static void withdraw(Account account, double sum, double companyOverdraftDiscount) {
+        Money accountMoney = account.getMoney();
+        double left = accountMoney.getValue() - sum;
+        accountMoney.setValue(accountMoney.getValue() < 0
+                ? left - sum * account.overdraftFee() * companyOverdraftDiscount
+                : left);
+    }
+}
