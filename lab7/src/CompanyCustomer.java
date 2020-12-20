6039caf4f1e4065522ff60d682dde0c933feba28
Dmytro Lavryk <dlavryk1011@gmail.com>
2020-12-20 17:12:20 +0200

    Adding new class CompanyCustomer. Deleting AccountTest.java and CustomerTest.java

---
+++ lab7/src/CompanyCustomer.java
@@ -0,0 +1,24 @@
+public class CompanyCustomer {
+    private double companyOverdraftDiscount;
+
+    public CompanyCustomer(String name, String email, Account account, double companyOverdraftDiscount) {
+        super(name,
+                email,
+                account,
+                CustomerType.COMPANY);
+        this.companyOverdraftDiscount = companyOverdraftDiscount;
+    }
+
+    public double getCompanyOverdraftDiscount() {
+        return companyOverdraftDiscount;
+    }
+
+    public void setCompanyOverdraftDiscount(double companyOverdraftDiscount) {
+        this.companyOverdraftDiscount = companyOverdraftDiscount;
+    }
+
+    @Override
+    public double getOverdraftDiscount() {
+        return companyOverdraftDiscount / (getAccount().isPremium() ? 2 : 1);
+    }
+}
